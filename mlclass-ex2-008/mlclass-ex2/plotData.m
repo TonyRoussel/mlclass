45deac121c4cb1b3ff8170306a397e1511961954
shortyp <tonydraf@gmail.com>
2015-02-11 14:01:52 +0100

    plotData && sigmoid done

--- mlclass-ex2-008/mlclass-ex2/plotData.m
+++ mlclass-ex2-008/mlclass-ex2/plotData.m
@@ -10,8 +10,15 @@ figure; hold on;
 % Instructions: Plot the positive and negative examples on a
 %               2D plot, using the option 'k+' for the positive
 %               examples and 'ko' for the negative examples.
-%
+%%
 
+% find indices of positive && negative examples
+pos = find(y == 1);
+neg = find(y == 0);
+
+% plot examples
+plot(X(pos, 1), X(pos, 2), 'k+', 'LineWidth', 2, 'MarkerSize', 7);
+plot(X(neg, 1), X(neg, 2), 'ko', 'MarkerFaceColor', 'y', 'MarkerSize', 7);
 
 
 
